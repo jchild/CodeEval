9f423a8d76585083e18485caddfdf1739bdaf40e
Jonathan Child <jchild45@gmail.com>
2015-03-25 15:10:04 +0800

    Create CapPercent.java

---
+++ CapPercent.java
@@ -0,0 +1,41 @@
+/*
+ * created By Jonathan Child
+ * Feb/12/2015
+ */
+
+import java.io.*;
+import java.util.*;
+
+
+public class CapPercent {
+	public static void main(String[] args) throws IOException {
+		Scanner file = new Scanner(new FileReader(args[0]));
+		while(file.hasNextLine()){
+			 processLine(file.nextLine());			
+		}
+	}
+
+
+	public static void processLine(String line){
+	
+		double lower = 0;
+		double upper = 0;
+		double totalLetters = 0;
+	
+		for(int i = 0; i< line.length(); i++){
+		
+			if(Character.isUpperCase(line.charAt(i))){
+				upper += 1;
+				totalLetters += 1;
+			}
+			else if (Character.isLowerCase(line.charAt(i))){
+				lower += 1;
+				totalLetters += 1;
+			}
+		
+		}
+		
+		System.out.printf("lowercase: %.2f uppercase: %.2f\n", (lower/totalLetters)*100, (upper/totalLetters)*100);
+
+	}
+}
