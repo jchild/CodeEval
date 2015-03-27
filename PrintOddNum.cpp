50370333d294b4147135566f244f3e0ceab9ed11
Jonathan Child <jchild45@gmail.com>
2015-03-27 17:18:20 +0800

    Create PrintOddNum.cpp

---
+++ PrintOddNum.cpp
@@ -0,0 +1,13 @@
+/*
+Print the odd numbers from 1 to 99.
+*/
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+int main(){
+	for (int i = 1; i < 99; i += 2){
+		cout << i << endl;
+	}
+	return 0;
+}
